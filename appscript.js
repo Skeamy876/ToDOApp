a2090e68ea3010835ef2858ec70797fab043500f
Ackeem Mclennon <awinstonmclennon@gmail.com>
2020-12-29 13:07:00 -0500

    Add Functon operational-listing pedning

--- appscript.js
+++ appscript.js
@@ -1,13 +1,13 @@
-var Events=[];
+var events=[];
 function Add(){
     var event=document.getElementById("event");
 
-    var s= Events.includes(event);
+    var s= events.includes(event);
 
     if (s==true){
         alert("Activity already added")
     }else{
-        Events.push(event)
+        events.push(event)
     }
+    console.log(events);
 }
-console.log(Events)
