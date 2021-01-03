7232560c69c541ffb838b683f2a5fede55baec3d
Ackeem Mclennon <awinstonmclennon@gmail.com>
2021-01-03 13:33:59 -0500

    Add and remove functions working- CSS styling pending

--- appscript.js
+++ appscript.js
@@ -6,10 +6,18 @@ function Add(){
         alert("Activity already added")
     }else{
         lists.push(list)
-        console.log(lists) 
-    }
-    var output=lists.join('<br/>');
-    document.getElementById("sec").innerHTML=output;
+        console.log(lists)
+        document.getElementById("sec").innerHTML += `<li idx="${lists.length-1}">${lists} <button type="button" onclick="remove()">Delete</button></li>`
+    }     
+    
 }
+//delete activity from list
+function remove(){
+    const btn = event.target;
+    const index = parseInt(btn.parentElement.getAttribute("idx"), 10);
+    activities.splice(index, 1);
+    document.getElementById("sec").innerHTML = lists.map((list, i) => {
+      return `<li idx="${i}">${lists} <button type="button" onclick="remove()">Delete</button></li>`
+    });
 
-
+}
