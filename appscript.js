4c6ca3767158d9f36d1c661988cdcc2c07a13ffe
Ackeem Mclennon <awinstonmclennon@gmail.com>
2021-01-03 13:34:47 -0500

    Add and Remove functions working- Css styling pending

--- appscript.js
+++ appscript.js
@@ -7,7 +7,7 @@ function Add(){
     }else{
         lists.push(list)
         console.log(lists)
-        document.getElementById("sec").innerHTML += `<li idx="${lists.length-1}">${lists} <button type="button" onclick="remove()">Delete</button></li>`
+        document.getElementById("sec").innerHTML += `<li idx="${lists.length-1}">${lists}<button type="button" onclick="remove()">Delete</button></li>`
     }     
     
 }
@@ -15,9 +15,8 @@ function Add(){
 function remove(){
     const btn = event.target;
     const index = parseInt(btn.parentElement.getAttribute("idx"), 10);
-    activities.splice(index, 1);
+    lists.splice(index, 1);
     document.getElementById("sec").innerHTML = lists.map((list, i) => {
-      return `<li idx="${i}">${lists} <button type="button" onclick="remove()">Delete</button></li>`
+      return `<li idx="${i}">${lists}<button type="button" onclick="remove()">Delete</button></li>`
     });
-
 }
